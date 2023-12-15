fca6de3763e08765dd23508cd50f104613394224
mutaman97 <mutamanelhadi97@gmail.com>
2023-12-14 16:17:25 -0800

    remove broken manipulation

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,43 +1,74 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 
 /**
- * free_listint_safe - function that frees a listint_t list
- * @h: double pointer to the struct listint_t
  *
- * Return: size_t, number of nodes in the linked list
- */
+ *  * free_listint_safe - frees a linked list
+ *
+ *   * @h: pointer to the first node in the linked list
+ *
+ *    *
+ *
+ *     * Return: number of elements in the freed list
+ *
+ *      */
 
 size_t free_listint_safe(listint_t **h)
+
 {
-	int diff = 0;
-	size_t count;
-	listint_t *temp;
-	listint_t *current;
 
-	if (h == NULL)
-                return (0);
-        if (*h == NULL)
-                return (0);
+	 size_t len = 0;
 
-	current = *h;
+	  int diff;
 
-	count = 0;
-	while (current != NULL)
-	{
-		count++;
-		diff = current - current->next;
-		if (diff > 0)
-		{
-			temp = current;
-			current = current->next;
-			free(temp);
-		}
-		else
-			break;
-	}
-	*head = NULL;
-	return (count);
+	   listint_t *temp;
+
+
+	    if (!h || !*h)
+
+		     return (0);
+
+
+	     while (*h)
+
+		      {
+
+			       diff = *h - (*h)->next;
+
+			        if (diff > 0)
+
+					 {
+
+						  temp = (*h)->next;
+
+						   free(*h);
+
+						    *h = temp;
+
+						     len++;
+
+						      }
+
+				 else
+
+					  {
+
+						   free(*h);
+
+						    *h = NULL;
+
+						     len++;
+
+						      break;
+
+						       }
+
+				  }
+
+
+	      *h = NULL;
+
+
+	       return (len);
+
 }
